0ff92945c2f65433434562ea9cf58e8c86b351e7
tramanh87 <ndtu96@gmail.com>
2017-06-05 16:30:28 +0700

    Thêm chức năng thêm , sửa nhà cung cấp

--- Quan_Ly_Kho/Quan_Ly_Kho/frm/frmNhaCungCap.Designer.cs
+++ Quan_Ly_Kho/Quan_Ly_Kho/frm/frmNhaCungCap.Designer.cs
@@ -28,11 +28,241 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtEmp_nm = new System.Windows.Forms.TextBox();
+            this.txtEmp_ctc = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.txtEmp_add = new System.Windows.Forms.TextBox();
+            this.txtEmp = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Gv = new System.Windows.Forms.DataGridView();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button3 = new System.Windows.Forms.Button();
+            this.btnXoa = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.Gv)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(153, 162);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(209, 22);
+            this.textBox1.TabIndex = 37;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(48, 167);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(68, 17);
+            this.label6.TabIndex = 36;
+            this.label6.Text = "Tìm kiếm:";
+            // 
+            // txtEmp_nm
+            // 
+            this.txtEmp_nm.Location = new System.Drawing.Point(153, 89);
+            this.txtEmp_nm.Name = "txtEmp_nm";
+            this.txtEmp_nm.Size = new System.Drawing.Size(209, 22);
+            this.txtEmp_nm.TabIndex = 30;
+            // 
+            // txtEmp_ctc
+            // 
+            this.txtEmp_ctc.Location = new System.Drawing.Point(587, 59);
+            this.txtEmp_ctc.Name = "txtEmp_ctc";
+            this.txtEmp_ctc.Size = new System.Drawing.Size(209, 22);
+            this.txtEmp_ctc.TabIndex = 31;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(587, 129);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(209, 22);
+            this.textBox2.TabIndex = 32;
+            // 
+            // txtEmp_add
+            // 
+            this.txtEmp_add.Location = new System.Drawing.Point(153, 126);
+            this.txtEmp_add.Name = "txtEmp_add";
+            this.txtEmp_add.Size = new System.Drawing.Size(209, 22);
+            this.txtEmp_add.TabIndex = 34;
+            // 
+            // txtEmp
+            // 
+            this.txtEmp.Location = new System.Drawing.Point(153, 57);
+            this.txtEmp.Name = "txtEmp";
+            this.txtEmp.Size = new System.Drawing.Size(209, 22);
+            this.txtEmp.TabIndex = 35;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(61, 129);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(55, 17);
+            this.label3.TabIndex = 24;
+            this.label3.Text = "Địa chỉ:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(486, 129);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(78, 17);
+            this.label7.TabIndex = 26;
+            this.label7.Text = "Mã số thuế";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(31, 91);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(123, 17);
+            this.label2.TabIndex = 25;
+            this.label2.Text = "Tên nhà cung cấp";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(486, 63);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(95, 17);
+            this.label4.TabIndex = 28;
+            this.label4.Text = "Số điện thoại:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(37, 57);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(117, 17);
+            this.label1.TabIndex = 29;
+            this.label1.Text = "Mã nhà cung cấp";
+            // 
+            // Gv
+            // 
+            this.Gv.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.Gv.GridColor = System.Drawing.Color.White;
+            this.Gv.Location = new System.Drawing.Point(12, 200);
+            this.Gv.Name = "Gv";
+            this.Gv.RowTemplate.Height = 24;
+            this.Gv.Size = new System.Drawing.Size(886, 297);
+            this.Gv.TabIndex = 23;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.button3);
+            this.panel1.Controls.Add(this.btnXoa);
+            this.panel1.Location = new System.Drawing.Point(-66, -36);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(898, 36);
+            this.panel1.TabIndex = 22;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(675, 3);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(98, 30);
+            this.button3.TabIndex = 0;
+            this.button3.Text = "Lưu";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // btnXoa
+            // 
+            this.btnXoa.Location = new System.Drawing.Point(779, 3);
+            this.btnXoa.Name = "btnXoa";
+            this.btnXoa.Size = new System.Drawing.Size(98, 30);
+            this.btnXoa.TabIndex = 0;
+            this.btnXoa.Text = "Xóa";
+            this.btnXoa.UseVisualStyleBackColor = true;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.button1);
+            this.panel2.Controls.Add(this.button2);
+            this.panel2.Location = new System.Drawing.Point(12, 12);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(898, 36);
+            this.panel2.TabIndex = 38;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(675, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(98, 30);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Lưu";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(779, 3);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(98, 30);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "Xóa";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // frmNhaCungCap
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(939, 504);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtEmp_nm);
+            this.Controls.Add(this.txtEmp_ctc);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.txtEmp_add);
+            this.Controls.Add(this.txtEmp);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Gv);
+            this.Controls.Add(this.panel1);
+            this.Name = "frmNhaCungCap";
             this.Text = "frmNhaCungCap";
+            ((System.ComponentModel.ISupportInitialize)(this.Gv)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtEmp_nm;
+        private System.Windows.Forms.TextBox txtEmp_ctc;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox txtEmp_add;
+        private System.Windows.Forms.TextBox txtEmp;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView Gv;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button btnXoa;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
--- Quan_Ly_Kho/Quan_Ly_Kho/frm/frmNhaCungCap.cs
+++ Quan_Ly_Kho/Quan_Ly_Kho/frm/frmNhaCungCap.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -15,6 +16,38 @@ namespace Quan_Ly_Kho.frm
         public frmNhaCungCap()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection(@"Data Source=DESKTOP-PF25KAL\Kevin;Initial Catalog=QLKhoHang;Integrated Security=True");
+            con.Open();
+            string sql = "";
+            if (txtEmp.Text != "")
+            {
+                sql = @"update nhacungcap set ten = N'" + txtEmp_nm.Text + "' ,diachi = N'" + txtEmp_add.Text + "',masothue = N'" + textBox2.Text + "' ,sdt = N'" + txtEmp_ctc.Text + "' where ma = N'" + txtEmp.Text + "'";
+            }
+            else
+            {
+
+                sql = @"insert into nhacungcap (ten,diachi,sdt,masothue) values (N'" + txtEmp_nm.Text + "',N'" + txtEmp_add.Text + "',N'" + txtEmp_ctc.Text + "',N'" + textBox2.Text + "')";
+
+            }
+            SqlCommand cmd = new SqlCommand(sql, con);
+            try
+            {
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("thanh cong");
+
+                con.Close();
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+                con.Close();
+            }
+
         }
     }
 }
